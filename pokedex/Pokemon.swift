df1ba9e94b28fbba4349164e22ee9853767215ba
Aleksander <aleksanderszymczak@gmail.com>
2017-01-23 16:59:20 +0100

    create pokemon class

---
+++ pokedex/Pokemon.swift
@@ -0,0 +1,31 @@
+//
+//  pokemon.swift
+//  pokedex
+//
+//  Created by olos on 23.01.2017.
+//  Copyright © 2017 olos. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    
+    var name: String {
+        
+        return _name
+    }
+    
+    var pokedexId: Int {
+        
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+}
