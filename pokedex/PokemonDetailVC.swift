5911df3eaad1f1d16a8a8a293bd17a81e04cdc3f
Aleksander <aleksanderszymczak@gmail.com>
2017-01-31 18:44:48 +0100

    end fo this part

---
+++ pokedex/PokemonDetailVC.swift
@@ -0,0 +1,26 @@
+//
+//  PokemonDetailVC.swift
+//  pokedex
+//
+//  Created by olos on 31.01.2017.
+//  Copyright © 2017 olos. All rights reserved.
+//
+
+import UIKit
+
+class PokemonDetailVC: UIViewController {
+    
+    var pokemon: Pokemon!
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+
+}
--- pokedex/ViewController.swift
+++ pokedex/ViewController.swift
@@ -101,6 +101,18 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        var poke: Pokemon!
+        
+        if inSearchMode {
+            
+            poke = filteredPokemon[indexPath.row]
+        } else {
+            
+            poke = pokemon[indexPath.row]
+        }
+        
+        performSegue(withIdentifier: "PokemonDetailVC", sender: poke)
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -154,6 +166,21 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
             filteredPokemon = pokemon.filter({$0.name.range(of: lower) != nil })
             collection.reloadData()
         }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "PokemonDetailVC" {
+            
+            if let detailVC = segue.destination as? PokemonDetailVC {
+                if let poke = sender as? Pokemon {
+                    detailVC.pokemon = poke
+                }
+                
+            }
+        }
+        
+        
     }
     
 
