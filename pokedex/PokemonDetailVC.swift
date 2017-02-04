469d2d6d73252cbdb00d9dc890570e7e56c48263
Aleksander <aleksanderszymczak@gmail.com>
2017-02-04 12:36:50 +0100

    outlets done

--- pokedex/PokemonDetailVC.swift
+++ pokedex/PokemonDetailVC.swift
@@ -12,6 +12,24 @@ class PokemonDetailVC: UIViewController {
     
     var pokemon: Pokemon!
     
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var pokedexidLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var attackLbl: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    @IBOutlet weak var evoLbl: UILabel!
+    
+    
+    
+    
+    
+    
     
     
 
@@ -21,6 +39,10 @@ class PokemonDetailVC: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func backBtnPressed(_ sender: UIButton) {
+        
+        dismiss(animated: true, completion: nil)
+    }
 
 
 }
